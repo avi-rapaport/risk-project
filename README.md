6911cef8cda78befef328df1ea70a09255ca5094
avi rapaport <kenblock.ar@gmail.com>
2026-08-24 12:42:07 +0300

    handled computer turn functions

--- README.md
+++ README.md
@@ -1,6 +1,47 @@
 # risk-project
 
+## files structure
+
+```
+├── Dockerfile
+├── README.md
+├── docker-compose.yaml
+├── exam-backend.md
+├── exam-links.md
+├── package-lock.json
+├── package.json
+├── public
+│   ├── app.js
+│   ├── assets
+│   │   ├── israel-lebanon-map-clean.png
+│   │   └── israel-lebanon-map.png
+│   ├── index.html
+│   └── styles.css
+└── src
+    ├── db.js
+    ├── middleware.js
+    ├── repositories
+    │   ├── game.repo.js
+    │   └── map.repo.js
+    ├── routes
+    │   └── game.route.js
+    ├── server.js
+    ├── services
+    │   ├── computer.service.js
+    │   ├── game.service.js
+    │   └── map.json
+    └── utils
+        └── utils.battle.js
+```
+
 ## DB choice
 
-I chose MongoDB because I think it is more flexible in terms of
-data structure and allows working with nested arrays and objects.
+I chose MongoDB because I think that its more flexible data structure allows for better work with arrays and nested objects, as in the current project, and also because there is no particular need for a live connection between two entities, so I don't need a relational database.
+
+## running instructions
+
+git clone https://github.com/avi-rapaport/risk-project.git
+
+cd risk project
+
+docker compose up --build
--- src/services/computer.service.js
+++ src/services/computer.service.js
@@ -1,3 +1,5 @@
+import { router } from "../routes/game.route";
+
 export async function runComputerTurn(game) {
   const computerEvevts = [];
 
@@ -27,10 +29,10 @@ function handleComputerReinforce(game) {
   const playerTerritories = game.territories.filter(
     (t) => t.owner === "player",
   );
-  playerTerritories.sort((a, b) => {
+  const sortedTerritories = playerTerritories.sort((a, b) => {
     a.distanceFromComputerHQ - b.distanceFromComputerHQ;
   });
-  const minDistance = playerTerritories[0];
+  const minDistance = sortedTerritories[0];
   const isDefence = minDistance <= 2;
 
   const borderTerritories = [];
@@ -39,13 +41,13 @@ function handleComputerReinforce(game) {
   );
   for (const ter of computerTerritories) {
     for (const neighbor of ter.neighbors)
-      if (playerTerritories.includes(neighbor)) {
+      if (playerTerritories.map((t) => t.id).includes(neighbor)) {
         borderTerritories.push(ter);
         break;
       }
   }
 
-  const checkBorders = borderTerritories.sort((a, b) => {
+  const sortBorders = borderTerritories.sort((a, b) => {
     if (isDefence) {
       if (a.distanceFromComputerHQ !== b.distanceFromComputerHQ) {
         return a.distanceFromComputerHQ - b.distanceFromComputerHQ;
@@ -65,13 +67,30 @@ function handleComputerReinforce(game) {
     }
   });
 
-  const toReinforce = checkBorders[0];
+  const toReinforce = sortBorders[0];
   toReinforce.soldiers += 3;
 
   return { type: "reinforce", territoryId: toReinforce.id, soldiersAdded: 3 };
 }
 
 function handleComputerAttack(game) {
+  const computerTerritories = game.territories.filter(
+    (t) => t.owner === "computer",
+  );
+  const playerTerritories = game.territories.filter(
+    (t) => t.owner === "player",
+  );
+
+  const candidates = [];
+
+  if (!candidates) {
+    return;
+  }
+
+  return { type: "attack" };
+}
+
+function handleComputerMove(game) {
   const playerTerritories = game.territories.filter(
     (t) => t.owner === "player",
   );
@@ -84,17 +103,71 @@ function handleComputerAttack(game) {
     (t) => t.owner === "computer",
   );
 
-  const candidates = [];
+  let candidates = [];
 
   for (const from of computerTerritories) {
-    for (const neighbor of from.neighbors) {
+    let min = 1;
+    if (from.headquarters) {
+      min = 4;
+    }
+
+    if (
+      from.soldiers > min &&
+      from.neighbors.every((nId) => {
+        !playerTerritories.map((t) => t.id).includes(nId);
+      })
+    ) {
+      for (const neighbor of ter.neighbors) {
+        const to = computerTerritories.find((t) => t.id === neighbor);
+        if (to) {
+          const move = { from, to };
+          candidates.push(move);
+        } else {
+          continue;
+        }
+      }
+    } else {
+      continue;
     }
   }
+
+  if (!candidates) {
+    return;
+  }
+
+  if (isDefence) {
+    candidates = candidates.filter((move) => {
+      move.to.distanceFromComputerHQ < move.from.distanceFromComputerHQ;
+    });
+  } else {
+    candidates = candidates.filter((move) => {
+      move.to.distanceFromPlayerHQ < move.from.distanceFromPlayerHQ;
+    });
+  }
+
+  const sortCandidates = candidates.sort((a, b) => {
+    if (a.to.soldiers !== b.to.soldiers) {
+      return a.to.soldiers - b.to.soldiers;
+    } else if (a.to.id !== b.to.id) {
+      return a.to.id - b.to.id;
+    } else {
+      return a.from.id - b.from.id;
+    }
+  });
+
+  const sentSoldiers = toMove.from.headquarters
+    ? toMove.from.soldiers - 4
+    : toMove.from.soldiers - 1;
+
+  const toMove = sortCandidates[0];
+
+  toMove.from.soldiers -= sentSoldiers;
+  toMove.to.soldiers += sentSoldiers;
+
+  return {
+    type: "move",
+    toId: toMove.to.id,
+    fromId: toMove.from.id,
+    soldiers: sentSoldiers,
+  };
 }
-
-function handleComputerMove(game) {}
-
-const ar = [{ x: 4 }, { x: 2 }, { x: 3 }];
-
-const arr = ar.filter((t) => t.x % 2 === 0);
-console.log(arr);
